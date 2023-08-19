8b39d8f7210f0cdb0e80b072067e7bc2e8909f40
titi-lima <titisau@gmail.com>
2023-08-19 17:07:51 -0300

    😮

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { Container, ImageContainer } from "./style";
+import { Logo } from "../assets";
+import Image from "next/image";
+import { CustomText } from "../styles/globalComponents";
+import { api } from "@/utils/api";
+
+export default function Home() {
+  const hello = api.example.hello.useQuery({ text: "CITi!" });
+  return (
+    <Container>
+      <ImageContainer>
+        <Image src={Logo} alt="" />
+      </ImageContainer>
+      <CustomText bold white size="40px" margin="20px 0">
+        NextJS Boilerplate
+      </CustomText>
+      <CustomText regular white secondary>
+        Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
+        <strong>&hearts;</strong> by CITi
+      </CustomText>
+      <CustomText regular white secondary>
+        {hello.data?.greeting}
+      </CustomText>
+    </Container>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {},
+  };
+}
